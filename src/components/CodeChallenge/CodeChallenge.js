db05c18eb2f7fbe404630c29dd99fc0521f4d011
Mike Bifulco <mbifulco@live.com>
2018-09-11 14:22:27 -0400

    rendering markup in an iframe

--- src/components/CodeChallenge/CodeChallenge.js
+++ src/components/CodeChallenge/CodeChallenge.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './CodeChallenge.css';
 
-import { UnControlled as CodeMirror } from 'react-codemirror2';
+import { Controlled as CodeMirror } from 'react-codemirror2';
 
 require('codemirror/mode/xml/xml');
 require('codemirror/mode/javascript/javascript');
 
 
-const CodeChallenge = (/* props */) => {
-  return (
-    <CodeMirror
-      value="<h1>I ♥ react-codemirror2</h1>"
-      options={{
-        mode: 'xml',
-        theme: 'material',
-        lineNumbers: true,
-      }}
-      onChange={(editor, data, value) => {
-        console.log(editor, data, value);
-      }}
-    />
-  );
-};
+class CodeChallenge extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '<h1>I ♥ react-codemirror2</h1>',
+    };
+  }
+
+  handleOnBeforeChange = (editor, data, value) => {
+    this.setState({
+      code: value,
+    });
+  }
+
+  hanleOnChange = (editor, data, value) => {
+    console.log(editor, data, value);
+  }
+
+  render() {
+    const { code } = this.state;
+
+    const styles = {
+      container: {
+      },
+      iFrame: {
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        width: '100%',
+      },
+    };
+
+    return (
+      <React.Fragment>
+        <div style={styles.container}>
+          <CodeMirror
+            value={code}
+            options={{
+              mode: 'xml',
+              theme: 'material',
+              lineNumbers: true,
+            }}
+            onBeforeChange={this.handleOnBeforeChange}
+            onChange={this.hanleOnChange}
+          />
+          <iframe
+            srcDoc={code}
+            style={styles.iFrame}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default CodeChallenge;
