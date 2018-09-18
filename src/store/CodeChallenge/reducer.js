8bb44f991873cbd05fd6b8b701754f2411e4d942
Mike Bifulco <mbifulco@live.com>
2018-09-18 14:49:13 -0400

    change starting text

--- src/store/CodeChallenge/reducer.js
+++ src/store/CodeChallenge/reducer.js
@@ -1,7 +1,7 @@
 import * as types from './types';
 
 const initialState = {
-  code: '<h1>I ❤️ Gymnasium</h1>',
+  code: '<h1>I ❤ Gymnasium</h1>',
 };
 
 const CodeChallenge = (state = initialState, action) => {
