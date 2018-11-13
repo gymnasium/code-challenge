392d14bd03087fe344e20cf120c2bf7bb95e2c7f
Mike Bifulco <mbifulco@live.com>
2018-11-13 14:09:48 -0500

    configure very basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('this is the server');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+  res.send('hello, world');
+});
+
+app.listen(process.env.PORT || 4000);
+console.log(`API Server is running on port ${PORT}`);
