5bcda534118447ddd6fc7ae9e1324d1b14e43ec4
Mike Bifulco <mbifulco@live.com>
2018-11-20 14:50:48 -0500

    calculate a grade based on input and goal code

--- server/controllers/grades.controller.js
+++ server/controllers/grades.controller.js
@@ -4,9 +4,28 @@
  * @param {*} res 
  */
 const calculateGrade = (req, res) => {
+  const {
+    inputCode,
+    goalCode,
+  } = req;
+
+  let grade = 100;
+  console.dir(req.body);
+
+  if (!goalCode) {
+    // do something if there's no goal code supplied
+    res.status(400).json({ error:'No goal code supplied!' });
+    return;
+  }
+
+  if (!inputCode) {
+    // do something if there's no input code supplied
+    grade = 0;
+  }
+
   res.json({
     message: 'You\'re the best!',
-    grade: 87,
+    grade,
   });
 };
 
